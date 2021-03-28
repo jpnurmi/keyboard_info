a2136398e96ab056a90cd4c813d239c9a02f5e1e
J-P Nurmi <jpnurmi@gmail.com>
2021-03-29 00:31:51 +0200

    Implement for Android

--- android/src/main/kotlin/com/example/keyboard_layout/KeyboardLayoutPlugin.kt
+++ android/src/main/kotlin/com/example/keyboard_layout/KeyboardLayoutPlugin.kt
@@ -2,34 +2,66 @@ package com.example.keyboard_layout
 
 import androidx.annotation.NonNull
 
+import android.app.Activity
+import android.view.inputmethod.InputMethodInfo
+import android.view.inputmethod.InputMethodManager
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import java.util.Locale
 
-/** KeyboardLayoutPlugin */
-class KeyboardLayoutPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
+class KeyboardLayoutPlugin: FlutterPlugin, ActivityAware, MethodCallHandler {
   private lateinit var channel : MethodChannel
+  private var activity: Activity? = null
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "keyboard_layout")
     channel.setMethodCallHandler(this)
   }
 
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+
+  override fun onAttachedToActivity(@NonNull binding: ActivityPluginBinding) {
+    activity = binding.activity
+  }
+
+  override fun onReattachedToActivityForConfigChanges(@NonNull binding: ActivityPluginBinding) {
+    activity = binding.activity
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    activity = null
+  }
+
+  override fun onDetachedFromActivity() {
+    activity = null
+  }
+
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    if (call.method == "getKeyboardLayout") {
+      result.success(getKeyboardLayout())
     } else {
       result.notImplemented()
     }
   }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+  private fun getKeyboardLayout(): String {
+    val manager = activity?.getSystemService(Activity.INPUT_METHOD_SERVICE) as? InputMethodManager
+    val type = manager?.getCurrentInputMethodSubtype()
+    val lang = type?.getLanguageTag()
+    if (lang?.isNullOrEmpty() ?: true) {
+      val locale = Locale.getDefault()
+      var country = locale.getCountry()
+      if (country.isNullOrEmpty())
+        return locale.getLanguage()
+      return country
+    }
+    return lang ?: ""
   }
 }
