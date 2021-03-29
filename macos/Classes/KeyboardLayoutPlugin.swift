ae6efbcb1e67c9a3348acc2c5ad2dd844045564d
J-P Nurmi <jpnurmi@gmail.com>
2021-03-29 23:53:51 +0200

    Implement for macOS

--- macos/Classes/KeyboardLayoutPlugin.swift
+++ macos/Classes/KeyboardLayoutPlugin.swift
@@ -1,5 +1,6 @@
 import Cocoa
 import FlutterMacOS
+import InputMethodKit
 
 public class KeyboardLayoutPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -10,8 +11,14 @@ public class KeyboardLayoutPlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "getPlatformVersion":
-      result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
+    case "getKeyboardLayout":
+      let source = TISCopyCurrentKeyboardInputSource().takeUnretainedValue()
+      let value = TISGetInputSourceProperty(source, kTISPropertyInputSourceLanguages)
+      if (value != nil) {
+        let languages = Unmanaged<AnyObject>.fromOpaque(value!).takeUnretainedValue() as? Array<String>
+        result(languages?.first)
+      }
+      result(nil)
     default:
       result(FlutterMethodNotImplemented)
     }
