043471d3f3e8bbad0ca9e8d9da5b174f8b52332b
J-P Nurmi <jpnurmi@gmail.com>
2021-03-30 22:22:51 +0200

    Opt integration tests out of null-safety

--- example/integration_test/keyboard_layout_integration_test.dart
+++ example/integration_test/keyboard_layout_integration_test.dart
@@ -1,6 +1,7 @@
+// @dart=2.9
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-
 import 'package:keyboard_layout_example/main.dart' as app;
 
 void main() {
