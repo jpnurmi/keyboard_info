51b089629d64d5b62679066ffaff929eee25b234
J-P Nurmi <jpnurmi@gmail.com>
2021-09-02 11:46:42 +0200

    Fix analysis

--- lib/src/keyboard_info_method_channel.dart
+++ lib/src/keyboard_info_method_channel.dart
@@ -1,10 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:meta/meta.dart';
-
 import 'package:keyboard_info/src/keyboard_info.dart';
 import 'package:keyboard_info/src/keyboard_info_platform_interface.dart';
+import 'package:meta/meta.dart';
 
 // ignore_for_file: public_member_api_docs
 
